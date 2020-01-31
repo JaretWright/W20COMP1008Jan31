cda22f0bdc704cb255670935a3638a478c81384f
Jaret Wright <hkkA223>
2020-01-31 12:26:08 -0500

    Added the view, controller and launch

--- src/PictureViewController.java
+++ src/PictureViewController.java
@@ -1,2 +1,19 @@
-public class PictureViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PictureViewController implements Initializable {
+
+    @FXML
+    private ImageView imageView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        imageView.setImage(new Image("chipmunk.jpg"));
+    }
 }
